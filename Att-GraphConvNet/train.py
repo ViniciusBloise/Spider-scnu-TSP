b23f582d65236e1602f59efbe0fe4945cd92a1fa
ViniciusBloise <vinicius.bloise@gmail.com>
2022-10-07 19:05:40 +0200

    create train

---
+++ Att-GraphConvNet/train.py
@@ -0,0 +1,20 @@
+import os
+
+import numpy as np
+
+import torch
+
+from config import *
+from utils.process import *
+
+notebook_mode = True
+viz_mode = False
+
+# model-parameter
+config_path = "configs/tsp20.json"
+config = get_config(config_path)
+
+if viz_mode==False:
+
+    # tsp20--model
+    net = main(config, pretrained=False, patience=50, lr_scale=0.001, random_neighbor=False)
